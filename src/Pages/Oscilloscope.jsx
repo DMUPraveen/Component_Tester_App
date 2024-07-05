25a4029909a7b8ac78585960fcd144bbaf48ce45
DMUPraveen <dmu.praveen@gmail.com>
2024-07-05 17:40:18 +0530

    Added changing windows and vertical scaling

--- src/Pages/Oscilloscope.jsx
+++ src/Pages/Oscilloscope.jsx
@@ -5,10 +5,10 @@ import DrawChart from "../Utilities/Draw_Chart";
 import { SerialPortContext } from "../Pages/Parent";
 import { create_serial_port, destroy_serial_port, read_serial_port, write_serial_port } from "../Utilities/Serial_Port";
 import { requestPermission } from "@tauri-apps/api/notification";
-import { findEdgeAndShift } from "../Utilities/Analysis";
+import { findEdgeAndShift, findEdgeAndShiftLinear } from "../Utilities/Analysis";
 const STM32_VID = 1155;
-function VerticalSlider({ onChange }) {
-
+const MAX_TRIGGER_LEVEL = "3.3";
+function VerticalSlider({ onChange, verticalScale }) {
     if (onChange == null) {
         onChange = (x) => { console.log(x); };
     }
@@ -16,9 +16,10 @@ function VerticalSlider({ onChange }) {
         <div className="flex flex-col items-center h-5/6">
             <input
                 type="range"
-                min="-10"
-                max="10"
-                onChange={onChange}
+                min={verticalScale[0]}
+                max={verticalScale[1]}
+                step="0.01"
+                onChange={(x) => { onChange(parseFloat(x.target.value)) }}
                 className="w-2 h-full accent-zinc-950 rounded-lg"
                 style={{
                     writingMode: 'bt-lr',
@@ -29,6 +30,56 @@ function VerticalSlider({ onChange }) {
         </div>
     );
 }
+function InformationPanel({ triggerLevel, triggerType, setTriggerType, windowLengthRef, verticalScaleRef, setVerticalScale }) {
+    let [mid, setMid] = useState(MAX_TRIGGER_LEVEL / 2);
+    let [range, setRange] = useState(MAX_TRIGGER_LEVEL);
+
+    const setLimits = (mid, range) => {
+        console.log(mid, range);
+        setVerticalScale([mid - range / 2, mid + range / 2]);
+    }
+    const setMidLimit = (e, range) => {
+        let val = parseFloat(e.target.value);
+        setMid(val);
+        setLimits(val, range);
+    }
+    const setRangeLimit = (e, mid) => {
+        let val = parseFloat(e.target.value);
+        setRange(val);
+        setLimits(mid, val);
+    }
+    const toggleTriggerType = () => {
+        if (triggerType == "rising") {
+            setTriggerType("falling");
+            return;
+        }
+        setTriggerType("rising");
+    }
+    return (
+        <div className="flex flex-col self-start py-10 px-3 gap-5 items-start">
+            <div className="h-1/5 bg-cyan-400 grow"> </div>
+            <p className="font-black">Trigger Level: {triggerLevel.toFixed(2)}</p>
+            <button onClick={toggleTriggerType} className="font-black shadow-md p-3 border-black border-2 rounded-md">Trigger: {triggerType}</button>
+            <div>
+                <p className="font-black">Window Size: </p>
+                <select onChange={(e) => { windowLengthRef.current = parseInt(e.target.value); console.log(e.target.value); }}
+                    className="font-black border-2 border-zinc-950 col-span-2">
+                    {/* <option value="1024">1024</option> */}
+                    <option value="512">512</option>
+                    <option value="256">256</option>
+                    <option value="128">128</option>
+                </select>
+            </div>
+            <div>
+                <p className="font-black">Vertical Scale Mid: </p>
+                <input onChange={(e) => { setMidLimit(e, range) }} />
+                <p className="font-black">Vertical Scale: </p>
+                <input onChange={(e) => { setRangeLimit(e, mid) }} />
+            </div>
+        </div>
+    )
+
+}
 export default function Oscilloscope() {
     const canvasRef = useRef(null);
     const x_val = Array.from({ length: 1024 }, (_, i) => i);
@@ -36,6 +87,27 @@ export default function Oscilloscope() {
     const [port, setSerialPort] = useState(null);
     const [reader, setReader] = useState(null);
     const data_buffer = useRef(new Uint8Array(new ArrayBuffer(1024 * 2)));
+    const [triggerLevel, setTriggerLevelProto] = useState(2.5);
+    const [triggerType, setTriggerTypeProto] = useState("rising");
+    const triggerRef = useRef(triggerLevel);
+    const triggerTypeRef = useRef(triggerType);
+    const windowLengthRef = useRef(512);
+    const [verticalScale, setVerticalScaleProto] = useState([0, 3.3]);
+    const verticalScaleRef = useRef(verticalScale);
+    // const [windowLength, setWindowLength] = us(1024);
+    function setTriggerLevel(newLevel) {
+        triggerRef.current = newLevel;
+        setTriggerLevelProto(newLevel);
+    }
+    function setTriggerType(newType) {
+        setTriggerTypeProto(newType);
+        triggerTypeRef.current = newType;
+    }
+    function setVerticalScale(newScale) {
+        verticalScaleRef.current = newScale;
+        setVerticalScaleProto(newScale);
+    }
+
 
     function start_serial_reader() {
 
@@ -77,9 +149,15 @@ export default function Oscilloscope() {
             // console.log(data.length);
             let y_val = Array.from(data, (byte) => byte / 4096 * 3.3);
             let x_val = Array.from({ length: 1024 }, (_, i) => i);
-            y_val = findEdgeAndShift(y_val, 2.5, "rising");
+            y_val = findEdgeAndShiftLinear(y_val, triggerRef.current, triggerTypeRef.current);
+            console.log(windowLengthRef.current)
+            x_val = x_val.slice(0, windowLengthRef.current);
+            y_val = y_val.slice(0, windowLengthRef.current);
             chartRef.current.data.labels = x_val;
             chartRef.current.data.datasets[0].data = y_val;
+            chartRef.current.options.scales.y.max = verticalScaleRef.current[1];
+            chartRef.current.options.scales.y.min = verticalScaleRef.current[0];
+            // console.log(verticalScaleRef.current);
             chartRef.current.update('none');
             setTimeout(read_serial, 100);
         }
@@ -105,22 +183,22 @@ export default function Oscilloscope() {
 
         <>
             <div className="font-black text-4xl p-3"> Oscilloscope </div>
-            <div className="flex flex-col items-center  p-3  h-screen ">
+            <div className="flex flex-col items-center  p-3  h-full ">
                 <button
-                    className="font-black text-2xl drop-shadow-l bg-white p-3 border-2 border-zinc-500"
+                    className="font-black text-2xl drop-shadow-l bg-white p-3 border-2 border-zinc-500 rounded-md"
                     onClick={start_stop_oscilloscope}
 
                 >
                     {(port == null) ? "Start" : "Stop"}
                 </button>
-                <div className="flex flex-row items-center justify-center  grow self-stretch">
-                    <VerticalSlider />
+                <div className="flex flex-row items-center justify-center gap-3 grow self-stretch">
+                    <InformationPanel triggerLevel={triggerLevel} triggerType={triggerType} setTriggerType={setTriggerType} windowLengthRef={windowLengthRef} verticalScaleRef={verticalScaleRef} setVerticalScale={setVerticalScale} />
+                    <VerticalSlider onChange={setTriggerLevel} verticalScale={verticalScale} />
                     <div className="w-10/12 p-3">
                         <canvas ref={canvasRef} ></canvas>
                     </div>
                 </div>
             </div>
-            {/* </div> */}
         </>
 
     )
--- src/Utilities/Analysis.js
+++ src/Utilities/Analysis.js
@@ -1,3 +1,14 @@
+function movingAverage(arr, windowSize) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        let start = Math.max(0, i - Math.floor(windowSize / 2));
+        let end = Math.min(arr.length, i + Math.floor(windowSize / 2) + 1);
+        let window = arr.slice(start, end);
+        let average = window.reduce((sum, val) => sum + val, 0) / window.length;
+        result.push(average);
+    }
+    return result;
+}
 export function findEdgeAndShift(arr, triggerValue, edgeType = 'rising') {
     let edgeIndex = -1;
 
@@ -31,5 +42,31 @@ export function findEdgeAndShift(arr, triggerValue, edgeType = 'rising') {
 
     return shiftedArray;
 }
+export function findEdgeAndShiftLinear(arr, triggerValue, edgeType) {
+    arr = movingAverage(arr, 3);
+    console.log(edgeType);
+    let startLimit = Math.floor(arr.length / 2);
+    if (edgeType == 'rising') {
+        for (let index = 0; index <= startLimit; index++) {
+            if (arr[index] < triggerValue && arr[index + 1] >= triggerValue) {
+                return arr.slice(index, index + startLimit);
+            }
+            // console.log(arr[index], arr[index + 1], triggerValue);
+
+        }
+        // throw new Error("Hooray")
+        // console.log("not found", arr[0], triggerValue);
+    }
+    else {
+        for (let index = 0; index <= startLimit; index++) {
+            if (arr[index] > triggerValue && arr[index + 1] <= triggerValue) {
+                return arr.slice(index, index + startLimit);
+            }
+        }
+    }
+    // console.log("I am running");
+    return arr.slice(startLimit, arr.length);
+}
+
 
 // Example usage:
