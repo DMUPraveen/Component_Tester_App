91c05240d7d6e1be84c191c542bc38a6c7e3a881
DMUPraveen <dmu.praveen@gmail.com>
2024-07-07 12:42:52 +0530

    Fully implemented Square Component

--- src/Pages/Signal_Generator.jsx
+++ src/Pages/Signal_Generator.jsx
@@ -3,6 +3,48 @@ import function_generator_logo from '../assets/function_generator_logo.png'
 import { arbitrary_wave, arbitrary_wave_extended, clamper, cosine_wave } from '../Utilities/Signal_Generator_func';
 import { SerialPortContext } from './Parent';
 import { read_serial_port, write_serial_port } from '../Utilities/Serial_Port';
+const MAX_AMPLITUDE = 3.3
+
+function SetupSquareWave() {
+    const [frequency, setFrequency] = useState(0);
+    const [amplitude, setAmplitude] = useState(0);
+    const { port, setPort } = useContext(SerialPortContext)
+    const square_func = (x) => {
+        let value = clamper(amplitude / MAX_AMPLITUDE)
+        if (x < 0.5) {
+            return value;
+        }
+        return 0;
+    }
+    return (
+        <>
+            <div className="font-black text-2xl col-span-2">Frequency (Hz) </div>
+            <div className="font-black text-2xl col-span-1">: </div>
+            <input type="number"
+                className="border-2 font-bold text-2xl border-zinc-950 col-span-2"
+                onChange={(e) => { setFrequency(e.target.value) }}
+                defaultValue={frequency}
+            />
+            <div className="font-black text-2xl col-span-2">Amplitude (V)</div>
+            <div className="font-black text-2xl col-span-1">: </div>
+            <input type="number"
+                className="border-2 font-bold text-2xl border-zinc-950 col-span-2"
+                onChange={(e) => { setAmplitude(e.target.value) }}
+                defaultValue={frequency}
+            />
+            <button className="w-2/3 font-bold 
+            rounded-lg text-2xl text-center 
+             text-white bg-zinc-950 hover:bg-slate-700 col-span-2"
+                onClick={async () => {
+                    let buffer = arbitrary_wave_extended(parseInt(frequency), square_func);
+                    await write_serial_port(port, buffer);
+                }}
+            >Generate</button>
+        </>
+    )
+
+}
+
 
 
 
@@ -70,21 +112,6 @@ function SetupSinewave() {
                     await write_serial_port(port, buffer);
                 }}
             >Generate</button>
-            {/* <button
-                className="w-2/3 font-bold 
-            rounded-lg text-2xl text-center 
-             text-white bg-zinc-950 hover:bg-slate-700 col-span-2"
-
-                onClick={async () => {
-                    if (port == null) return console.log("Port is null");
-                    let buffer = arbitrary_wave(parseInt(frequency), new Array(400));
-                    buffer = arbitrary_wave_extended(parseInt(frequency), (x) => 0.5 * Math.sin(x * 2 * Math.PI));
-                    console.log(buffer);
-                    await write_serial_port(port, buffer);
-                }}
-            >
-                Hello
-            </button> */}
         </>
     )
 }
@@ -104,6 +131,11 @@ function ConditionalOptions({ signalType }) {
             <>
                 <SetupArbitraryWave />
             </>
+        )
+    }
+    if (signalType === 'Square') {
+        return (
+            <SetupSquareWave />
         )
     }
     return (
